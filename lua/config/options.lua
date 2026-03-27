0100ef270a63236307b5cac3b9af3a9ba72ae08f
prowan <prowan@cargurus.com>
2026-03-26 23:00:06 -0400

    dark mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,26 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 12
+
+-- Enable the option to require a Prettier config file
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = false
+
+-- Set to false to disable auto format
+vim.g.lazyvim_eslint_auto_format = true
